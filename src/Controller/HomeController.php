2368b57bee1b6d807fe2414a629d653f7a6ee998
DenZaiyy <denzaiyy@outlook.fr>
2023-05-09 00:00:43 +0200

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Plateform;
+use App\Entity\Stock;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            //"tendances" =>
-        ]);
-    }
+	#[Route('/', name: 'app_home')]
+	public function index(EntityManagerInterface $em): Response
+	{
+		$tendencies = $em->getRepository(Stock::class)->findGamesInTendencies();
+		dd($tendencies);
+
+		return $this->render('home/index.html.twig', [
+			'tendencies' => $tendencies,
+		]);
+	}
 }
