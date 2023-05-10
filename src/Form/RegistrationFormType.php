dc41d2eabcec495a9d0b00486a11939790f59802
DenZaiyy <denzaiyy@outlook.fr>
2023-05-10 16:26:00 +0200

    Translate registerForm and add constraints
    ps: template\registration\register.html.twig need to be updated for dropzone style

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -21,20 +21,33 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('username', TextType::class, [
-				'label' => 'Pseudo'
+				'required' => true,
+				'attr' => [
+					'placeholder' => 'Pseudo',
+					'class' => 'form-control'
+				]
 			])
-            ->add('email', EmailType::class)
+            ->add('email', EmailType::class, [
+				'required' => true,
+				'attr' => [
+					'placeholder' => 'Email',
+					'class' => 'form-control'
+				]
+			])
             ->add('plainPassword', RepeatedType::class, [
 				'mapped' => false,
 				'type' => PasswordType::class,
-				'invalid_message' => 'The password fields must match.',
+				'invalid_message' => 'Les champs du mot de passe doivent correspondre.',
 				'options' => ['attr' => ['class' => 'password-field']],
 				'required' => true,
-				'first_options'  => ['label' => 'Mot de passe'],
-				'second_options' => ['label' => 'Confirmer votre mot de passe'],
+				'first_options'  => ['label' => 'Mot de passe', 'attr' => ['placeholder' => 'Mot de passe', 'class' => 'form-control']],
+				'second_options' => ['label' => 'Confirmer votre mot de passe', 'attr' => ['placeholder' => 'Confirmer votre mot de passe', 'class' => 'form-control']],
             ])
 			->add('avatar', DropzoneType::class, [
 				'label' => 'Avatar',
+				'attr' => [
+					'placeholder' => 'Glisser/déposer un fichier ou cliquer pour le chercher',
+				],
 				'mapped' => false,
 				'required' => false,
 				'constraints' => [
@@ -45,13 +58,22 @@ class RegistrationFormType extends AbstractType
 							'image/jpeg',
 							'image/png'
 						],
-						'mimeTypesMessage' => "Veuillez upload une image valide"
+						'mimeTypesMessage' => "Veuillez upload une image valide",
+						'extensions' => [
+							'jpg',
+							'jpeg',
+							'png'
+						],
+						'extensionsMessage' => 'Veuillez upload une image valide',
 					])
 				],
 			])
 			->add('agreeTerms', CheckboxType::class, [
 				'mapped' => false,
 				'label' => 'Accepter les conditions',
+				'attr' => [
+					'class' => 'form-check-input'
+				],
 				'constraints' => [
 					new IsTrue([
 						'message' => 'Vous devez accepter nos conditions générales.',
