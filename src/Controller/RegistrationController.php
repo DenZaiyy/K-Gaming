e205c2c75d457c017012fa12808c72306b90ac0d
DenZaiyy <denzaiyy@outlook.fr>
2023-05-10 14:51:28 +0200

    add FileUpload for avatar in register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\EmailVerifier;
+use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,7 +27,7 @@ class RegistrationController extends AbstractController
     }
 
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, FileUploader $fileUploader): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -40,6 +41,17 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
+
+			/** @var UploadedFile $avatarFile */
+			$avatarFile = $form->get('avatar')->getData();
+
+			if($avatarFile)
+			{
+				$avatarFileName = $fileUploader->upload($avatarFile);
+				$user->setAvatar($avatarFileName);
+			} else {
+				$user->setAvatar('img/default.png');
+			}
 
             $entityManager->persist($user);
             $entityManager->flush();
