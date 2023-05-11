9406ccaccb94f05fbf622121e04e0da20fbee29f
DenZaiyy <denzaiyy@outlook.fr>
2023-05-11 15:58:23 +0200

    update entity stock

--- src/Entity/Stock.php
+++ src/Entity/Stock.php
@@ -3,95 +3,101 @@
 namespace App\Entity;
 
 use App\Repository\StockRepository;
+use DateTimeInterface;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: StockRepository::class)]
 class Stock
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+	#[ORM\Id]
+	#[ORM\GeneratedValue]
+	#[ORM\Column]
+	private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $license_key = null;
+	#[ORM\Column(length: 255)]
+	private ?string $license_key = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    private ?\DateTimeInterface $date_availability = null;
+	#[ORM\Column(type: Types::DATETIME_MUTABLE)]
+	private ?DateTimeInterface $date_availability = null;
 
-    #[ORM\Column]
-    private ?bool $is_available = null;
+	#[ORM\Column]
+	private ?bool $is_available = null;
 
-    #[ORM\ManyToOne(inversedBy: 'stocks')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Game $game = null;
+	#[ORM\ManyToOne(inversedBy: 'stocks')]
+	#[ORM\JoinColumn(nullable: false)]
+	private ?Game $game = null;
 
-    #[ORM\ManyToOne(inversedBy: 'stock')]
-    private ?Purchase $purchase = null;
+	#[ORM\ManyToOne(inversedBy: 'stock')]
+	private ?Purchase $purchase = null;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	public function __toString(): string
+	{
+		return $this->license_key;
+	}
 
-    public function getLicenseKey(): ?string
-    {
-        return $this->license_key;
-    }
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
 
-    public function setLicenseKey(string $license_key): self
-    {
-        $this->license_key = $license_key;
+	public function getLicenseKey(): ?string
+	{
+		return $this->license_key;
+	}
 
-        return $this;
-    }
+	public function setLicenseKey(string $license_key): self
+	{
+		$this->license_key = $license_key;
 
-    public function getDateAvailability(): ?\DateTimeInterface
-    {
-        return $this->date_availability;
-    }
+		return $this;
+	}
 
-    public function setDateAvailability(\DateTimeInterface $date_availability): self
-    {
-        $this->date_availability = $date_availability;
+	public function getDateAvailability(): ?DateTimeInterface
+	{
+		return $this->date_availability;
+	}
 
-        return $this;
-    }
+	public function setDateAvailability(DateTimeInterface $date_availability): self
+	{
+		$this->date_availability = $date_availability;
 
-    public function isIsAvailable(): ?bool
-    {
-        return $this->is_available;
-    }
+		return $this;
+	}
 
-    public function setIsAvailable(bool $is_available): self
-    {
-        $this->is_available = $is_available;
+	public function isIsAvailable(): ?bool
+	{
+		return $this->is_available;
+	}
 
-        return $this;
-    }
+	public function setIsAvailable(bool $is_available): self
+	{
+		$this->is_available = $is_available;
 
-    public function getGame(): ?Game
-    {
-        return $this->game;
-    }
+		return $this;
+	}
 
-    public function setGame(?Game $game): self
-    {
-        $this->game = $game;
+	public function getGame(): ?Game
+	{
+		return $this->game;
+	}
 
-        return $this;
-    }
+	public function setGame(?Game $game): self
+	{
+		$this->game = $game;
 
-    public function getPurchase(): ?Purchase
-    {
-        return $this->purchase;
-    }
+		return $this;
+	}
 
-    public function setPurchase(?Purchase $purchase): self
-    {
-        $this->purchase = $purchase;
+	public function getPurchase(): ?Purchase
+	{
+		return $this->purchase;
+	}
 
-        return $this;
-    }
+	public function setPurchase(?Purchase $purchase): self
+	{
+		$this->purchase = $purchase;
+
+		return $this;
+	}
 }
