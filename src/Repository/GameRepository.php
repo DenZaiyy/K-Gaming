ebc2e44124f7f20348817c1de03105c889b684c5
DenZaiyy <denzaiyy@outlook.fr>
2023-05-12 17:00:19 +0200

    update: add new request

--- src/Repository/GameRepository.php
+++ src/Repository/GameRepository.php
@@ -39,40 +39,53 @@ class GameRepository extends ServiceEntityRepository
         }
     }
 
-	public function findGamesInPreOrders(): array
-	{
-		return $this->createQueryBuilder('g')
-			->select('g.id', 'g.label', 'g.price', 'g.date_release')
-			->where('g.date_release > :date')
-			->setParameter('date', new \DateTime())
-			->setMaxResults(3)
-			->getQuery()
-			->getResult()
-			;
-	}
+    public function findGamesInPreOrders(): array
+    {
+        return $this->createQueryBuilder('g')
+            ->select('g.id', 'g.label', 'g.price', 'g.date_release')
+            ->where('g.date_release > :date')
+            ->setParameter('date', new \DateTime())
+            ->setMaxResults(3)
+            ->getQuery()
+            ->getResult();
+    }
 
-//    /**
-//     * @return Game[] Returns an array of Game objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('g')
-//            ->andWhere('g.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('g.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findOneGameInPlatforms($gameID, $plateformID): array
+    {
+        return $this->createQueryBuilder('g')
+            ->select('g.id', 'g.label', 'g.price', 'g.date_release')
+            ->leftJoin('game_plateform', 'gp', 'ON', 'g.id = gp.game_id')
+            ->leftJoin('plateform', 'p', 'ON', 'p.id = gp.plateform_id')
+            ->where('gp.plateform_id = :plateformID')
+            ->andWhere('gp.game_id = :gameID')
+            ->setParameter('plateformID', $plateformID)
+            ->setParameter('gameID', $gameID)
+            ->getQuery()
+            ->getResult();
+    }
 
-//    public function findOneBySomeField($value): ?Game
-//    {
-//        return $this->createQueryBuilder('g')
-//            ->andWhere('g.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Game[] Returns an array of Game objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('g')
+    //            ->andWhere('g.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('g.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Game
+    //    {
+    //        return $this->createQueryBuilder('g')
+    //            ->andWhere('g.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
