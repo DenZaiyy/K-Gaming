64c41e0732acab3cfebd0e5d79c3fefaa01721e9
DenZaiyy <denzaiyy@outlook.fr>
2023-05-14 23:22:25 +0200

    update: add dark/light theme

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,45 @@
+let trans = () => {
+	document.documentElement.classList.add("transition");
+	window.setTimeout(() => {
+		document.documentElement.classList.remove("transition");
+	}, 1000)
+}
+
+$(document).ready(function () {
+	const $checkbox = $("#switch");
+	let $theme = localStorage.getItem("data-theme");
+	
+	document.documentElement.setAttribute("data-theme", $theme);
+	
+	if($theme == "dark") {
+		$checkbox.prop('checked', true)
+	} else {
+		$checkbox.prop('checked', false)
+	}
+	
+	const $changeThemeToDark = () => {
+		document.documentElement.setAttribute("data-theme", "dark");
+		localStorage.setItem("data-theme", "dark");
+        trans();
+	}
+	
+	const $changeThemeToLight = () => {
+		document.documentElement.setAttribute("data-theme", "light");
+		trans();
+		localStorage.setItem("data-theme", "light");
+	}
+	
+	$checkbox.change(function () {
+		if (this.checked) {
+			$changeThemeToDark()
+			$checkbox.prop('checked', true)
+		} else {
+			$changeThemeToLight()
+			$checkbox.prop('checked', false)
+		}
+	})
+})
+
 /*
 $(document).ready(function() {
 	$(".dropdown").each(function() {
