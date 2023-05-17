7f0f726ded3623717aa52f17f189cef5d3ae7f3e
DenZaiyy <denzaiyy@outlook.fr>
2023-05-17 17:34:44 +0200

    update: refacto service and request

--- src/Service/CallApiService.php
+++ src/Service/CallApiService.php
@@ -7,26 +7,58 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CallApiService extends AbstractController
 {
-	public function __construct(private HttpClientInterface $client)
+	public function __construct(private readonly HttpClientInterface $client)
 	{
-
 	}
 
-	public function getInfosGame($game): array
+    // Function to create connection with api (IGDB)
+    public function connectAPI($data) : array
+    {
+        $response = $this->client->request(
+            'POST',
+            'https://api.igdb.com/v4/games/',
+            [
+                'headers' => [
+                    'Client-ID' => $this->getParameter('app.client_id'),
+                    'Authorization' => $this->getParameter('app.authorization')
+                ],
+                'body' => $data
+            ]
+
+        );
+
+        return $response->toArray();
+    }
+
+    // function to get cover of a game and using the function connectAPI to connect to the api
+	public function getCoverByGame($game) : array
 	{
-		$response = $this->client->request(
-			'POST',
-			'https://api.igdb.com/v4/games/',
-			[
-				'headers' => [
-					'Client-ID' => $this->getParameter('app.client_id'),
-					'Authorization' => $this->getParameter('app.authorization')
-				],
-				'body' => 'fields id, name, cover.image_id, summary; where name = "' . $game . '";'
-			]
-
-		);
-
-		return $response->toArray();
+        return $this->connectAPI(
+            'fields id, name, cover.image_id; where name = "' . $game . '";'
+        );
 	}
+
+    // function to get the summary of a game and using the function connectAPI to connect to the api
+    public function getSummaryByGame($game) : array
+    {
+        return $this->connectAPI(
+            'fields name, summary; where name = "' . $game . '";'
+        );
+    }
+
+    // function to get screenshots of a game and using the function connectAPI to connect to the api
+    public function getScreenshotByGame($game) : array
+    {
+        return $this->connectAPI(
+            'fields name, screenshots.image_id; where name = "' . $game . '";'
+        );
+    }
+
+    // function to get videos of a game and using the function connectAPI to connect to the api
+    public function getVideosByGame($game) : array
+    {
+        return $this->connectAPI(
+            'fields name, videos.name, videos.video_id; where name = "' . $game . '";'
+        );
+    }
 }
