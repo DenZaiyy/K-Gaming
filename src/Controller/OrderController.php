397319db2cc43e248613eb76ca7bcf1c09eb095a
DenZaiyy <denzaiyy@outlook.fr>
2023-05-23 16:44:22 +0200

    add: (starting) order ctrl + view + formType

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\OrderType;
+use App\Service\CartService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderController extends AbstractController
+{
+    #[Route('/order', name: 'order_index')]
+    public function index(CartService $cartService): Response
+    {
+        if(!$this->getUser())
+        {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+
+        return $this->render('order/index.html.twig', [
+            'form' => $form->createView(),
+            'recapCart' => $cartService->getTotal()
+        ]);
+    }
+}
---
+++ src/Form/OrderType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $user = $options['user'];
+        $builder
+            ->add('addresses', EntityType::class, [
+                'class' => Address::class,
+                'label' => false,
+                'required' => true,
+                'multiple' => false,
+                'choices' => $user->getAddresses(),
+                'expanded' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'user' => []
+        ]);
+    }
+}
