308691c6e536cfa48c97d7e4626fb089770d757c
DenZaiyy <denzaiyy@outlook.fr>
2023-05-23 16:43:27 +0200

    up: add ctrl user and view for detail acc

---
+++ src/Controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/my-account', name: 'user_my_account')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
