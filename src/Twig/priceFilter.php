1989d0d05247532355b804a463faec448f1a4ccf
DenZaiyy <denzaiyy@outlook.fr>
2023-05-24 22:32:15 +0200

    add: filter twig for price

---
+++ src/Twig/priceFilter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class priceFilter extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('price', [$this, 'price'])
+        ];
+
+    }
+
+    public function price($price, string $symbol = '€', string $separator = ',', string $secondSeparator = ' '): string
+    {
+        $finalPrice = $price / 100;
+        $finalPrice = number_format($finalPrice, 2, $separator, $secondSeparator);
+
+        return $finalPrice . ' '. $symbol;
+    }
+}
