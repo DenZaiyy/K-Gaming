1a4eccf0777bd66844d4720d887980e045c6eee2
DenZaiyy <denzaiyy@outlook.fr>
2023-05-30 16:52:30 +0200

    up: fix mapping for collection games

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -10,68 +10,68 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: GenreRepository::class)]
 class Genre
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
-    #[ORM\Column(length: 50)]
-    private ?string $label = null;
-
-    #[ORM\ManyToMany(targetEntity: Game::class, mappedBy: 'genre')]
-    private Collection $games;
-
-    public function __toString(): string
-    {
-        return $this->label;
-    }
-
-    public function __construct()
-    {
-        $this->games = new ArrayCollection();
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getLabel(): ?string
-    {
-        return $this->label;
-    }
-
-    public function setLabel(string $label): self
-    {
-        $this->label = $label;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Game>
-     */
-    public function getGames(): Collection
-    {
-        return $this->games;
-    }
-
-    public function addGame(Game $game): self
-    {
-        if (!$this->games->contains($game)) {
-            $this->games->add($game);
-            $game->addGenre($this);
-        }
-
-        return $this;
-    }
-
-    public function removeGame(Game $game): self
-    {
-        if ($this->games->removeElement($game)) {
-            $game->removeGenre($this);
-        }
-
-        return $this;
-    }
+	#[ORM\Id]
+	#[ORM\GeneratedValue]
+	#[ORM\Column]
+	private ?int $id = null;
+	
+	#[ORM\Column(length: 50)]
+	private ?string $label = null;
+	
+	#[ORM\ManyToMany(targetEntity: Game::class, mappedBy: 'genres')]
+	private Collection $games;
+	
+	public function __toString(): string
+	{
+		return $this->label;
+	}
+	
+	public function __construct()
+	{
+		$this->games = new ArrayCollection();
+	}
+	
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
+	
+	public function getLabel(): ?string
+	{
+		return $this->label;
+	}
+	
+	public function setLabel(string $label): self
+	{
+		$this->label = $label;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return Collection<int, Game>
+	 */
+	public function getGames(): Collection
+	{
+		return $this->games;
+	}
+	
+	public function addGame(Game $game): self
+	{
+		if (!$this->games->contains($game)) {
+			$this->games->add($game);
+			$game->addGenre($this);
+		}
+		
+		return $this;
+	}
+	
+	public function removeGame(Game $game): self
+	{
+		if ($this->games->removeElement($game)) {
+			$game->removeGenre($this);
+		}
+		
+		return $this;
+	}
 }
