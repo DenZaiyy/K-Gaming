c9bcc17a24127454a361889c4a368badffee1ca0
DenZaiyy <denzaiyy@outlook.fr>
2023-05-31 12:00:19 +0200

    up: add request dql

--- src/Repository/StockRepository.php
+++ src/Repository/StockRepository.php
@@ -16,97 +16,114 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class StockRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Stock::class);
-    }
-
-    public function save(Stock $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function remove(Stock $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function findGamesInTendencies(): array
-    {
-        return $this->createQueryBuilder('s')
-            ->select('g.id', 'g.label', 'g.price', 'g.date_release')
-            ->leftJoin('s.game', 'g')
-            ->leftJoin('s.purchase', 'p')
-            ->Where('s.is_available = false')
-            ->andWhere('s.purchase IS NOT NULL')
-            ->groupBy('g.id', 'g.label', 'g.price', 'g.date_release')
-            ->orderBy('COUNT(s.id)', 'DESC')
-            ->setMaxResults(6)
-            ->getQuery()
-            ->getResult();
-    }
-
-    public function findStockByGameID($gameID): array
-    {
-        return $this->createQueryBuilder('s')
-            ->select('p.id AS platform_id', 'g.id AS game_id', 'g.label', 'COUNT(s.game) AS total', 'p.label AS platform_label', 'p.logo')
-            ->leftJoin('s.game', 'g')
-            ->leftJoin('s.plateform', 'p')
-            ->where('g.id = :gameID')
-            ->andwhere('s.is_available = true')
-            ->groupBy('g.label', 'p.label', 'p.logo', 'p.id')
-            ->setParameter('gameID', $gameID)
-            ->getQuery()
-            ->getResult();
-    }
-
-    public function findAvailableGameStockByPlatform($gameID, $platformID): array
-    {
-        return $this->createQueryBuilder('s')
-            ->select('p.id AS platform_id', 'g.id AS game_id', 'g.label', 'p.label AS platform_label', 'COUNT(s.game) AS total')
-            ->leftJoin('s.game', 'g')
-            ->leftJoin('s.plateform', 'p')
-            ->where('g.id = :gameID')
-            ->andWhere('p.id = :platformID')
-            ->andWhere('s.is_available = true')
-            ->setParameters([
-                'gameID' => $gameID,
-                'platformID' => $platformID
-            ])
-            ->getQuery()
-            ->getResult();
-    }
-
-    //    /**
-    //     * @return Stock[] Returns an array of Stock objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('s.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Stock
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+	public function __construct(ManagerRegistry $registry)
+	{
+		parent::__construct($registry, Stock::class);
+	}
+	
+	public function save(Stock $entity, bool $flush = false): void
+	{
+		$this->getEntityManager()->persist($entity);
+		
+		if ($flush) {
+			$this->getEntityManager()->flush();
+		}
+	}
+	
+	public function remove(Stock $entity, bool $flush = false): void
+	{
+		$this->getEntityManager()->remove($entity);
+		
+		if ($flush) {
+			$this->getEntityManager()->flush();
+		}
+	}
+	
+	public function findGamesInTendencies(): array
+	{
+		return $this->createQueryBuilder('s')
+			->select('g.id', 'g.label', 'g.price', 'g.date_release')
+			->leftJoin('s.game', 'g')
+			->leftJoin('s.purchase', 'p')
+			->Where('s.is_available = false')
+			->andWhere('s.purchase IS NOT NULL')
+			->groupBy('g.id', 'g.label', 'g.price', 'g.date_release')
+			->orderBy('COUNT(s.id)', 'DESC')
+			->setMaxResults(6)
+			->getQuery()
+			->getResult();
+	}
+	
+	public function findStockByGameID($gameID): array
+	{
+		return $this->createQueryBuilder('s')
+			->select('p.id AS platform_id', 'g.id AS game_id', 'g.label', 'COUNT(s.game) AS total', 'p.label AS platform_label', 'p.logo')
+			->leftJoin('s.game', 'g')
+			->leftJoin('s.plateform', 'p')
+			->where('g.id = :gameID')
+			->andwhere('s.is_available = true')
+			->groupBy('g.label', 'p.label', 'p.logo', 'p.id')
+			->setParameter('gameID', $gameID)
+			->getQuery()
+			->getResult();
+	}
+	
+	public function findAvailableGameStockByPlatform($gameID, $platformID): array
+	{
+		return $this->createQueryBuilder('s')
+			->select('p.id AS platform_id', 'g.id AS game_id', 'g.label', 'p.label AS platform_label', 'COUNT(s.game) AS total')
+			->leftJoin('s.game', 'g')
+			->leftJoin('s.plateform', 'p')
+			->where('g.id = :gameID')
+			->andWhere('p.id = :platformID')
+			->andWhere('s.is_available = true')
+			->setParameters([
+				'gameID' => $gameID,
+				'platformID' => $platformID
+			])
+			->getQuery()
+			->getResult();
+	}
+	
+	public function findLicenseKeyAvailableByGamesAndPlatform($gameID, $platformID): array
+	{
+		return $this->createQueryBuilder('s')
+			->select('s.license_key')
+			->leftJoin('s.game', 'g')
+			->leftJoin('s.plateform', 'p')
+			->where('g.id = :gameID')
+			->andWhere('p.id = :platformID')
+			->andWhere('s.is_available = true')
+			->setParameters([
+				'gameID' => $gameID,
+				'platformID' => $platformID
+			])
+			->getQuery()
+			->getResult();
+	}
+	
+	//    /**
+	//     * @return Stock[] Returns an array of Stock objects
+	//     */
+	//    public function findByExampleField($value): array
+	//    {
+	//        return $this->createQueryBuilder('s')
+	//            ->andWhere('s.exampleField = :val')
+	//            ->setParameter('val', $value)
+	//            ->orderBy('s.id', 'ASC')
+	//            ->setMaxResults(10)
+	//            ->getQuery()
+	//            ->getResult()
+	//        ;
+	//    }
+	
+	//    public function findOneBySomeField($value): ?Stock
+	//    {
+	//        return $this->createQueryBuilder('s')
+	//            ->andWhere('s.exampleField = :val')
+	//            ->setParameter('val', $value)
+	//            ->getQuery()
+	//            ->getOneOrNullResult()
+	//        ;
+	//    }
 }
