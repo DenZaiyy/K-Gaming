365679e73c7b58db9ba4e2f93442da4fe7715517
DenZaiyy <denzaiyy@outlook.fr>
2023-05-31 11:59:41 +0200

    up: paymentController

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -13,14 +13,16 @@ use Stripe\Checkout\Session;
 use Stripe\Stripe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class PaymentController extends AbstractController
 {
 	//Constructor to get the EntityManagerInterface and the UrlGeneratorInterface
-	public function __construct(private EntityManagerInterface $em, private UrlGeneratorInterface $urlGenerator)
+	public function __construct(private EntityManagerInterface $em, private UrlGeneratorInterface $urlGenerator, private RequestStack $requestStack)
 	{
 	}
 	
@@ -51,7 +53,7 @@ class PaymentController extends AbstractController
 		}
 		
 		foreach ($purchase->getRecapDetails()->getValues() as $product) {
-			$productData = $this->em->getRepository(Game::class)->findOneBy(['label' => $product->getGame()]);
+			$productData = $this->em->getRepository(Game::class)->findOneBy(['label' => $product->getGameLabel()]);
 			$amount = str_replace('.', '', $productData->getPrice());
 			
 			$productStripe[] = [
@@ -59,7 +61,7 @@ class PaymentController extends AbstractController
 					'currency' => 'eur',
 					'unit_amount' => $amount,
 					'product_data' => [
-						'name' => $product->getGame(),
+						'name' => $product->getGameLabel(),
 					],
 				],
 				'quantity' => $product->getQuantity(),
@@ -89,25 +91,66 @@ class PaymentController extends AbstractController
 				'enabled' => true,
 			],
 		]);
+//		dd($checkout_session);
 		
-		$purchase->setStripeSessionId($checkout_session->id);
-		$this->em->flush();
+		$stripeID = $this->requestStack->getSession()->get('stripeID', []);
+		$stripeID[] = $checkout_session->id;
+		$this->requestStack->getSession()->set('stripeID', $stripeID);
+		
+		
+		if ($checkout_session->payment_status === 'paid') {
+			$purchase->setStripeSessionId($checkout_session->id);
+			$this->em->flush();
+		}
 		
 		return new RedirectResponse($checkout_session->url, 303);
 	}
 	
 	//Function to redirect to the success page
 	#[Route('/order/success/{reference}', name: 'payment_success')]
-	public function stripeSuccess($reference, CartService $cartService): Response
+	public function purchaseSuccess($reference, CartService $cartService): Response
 	{
+		$purchase = $this->em->getRepository(Purchase::class)->findOneBy(['reference' => $reference]);
+		
+		if ($purchase->getMethod() === 'stripe') {
+			$stripeID = $this->requestStack->getSession()->get('stripeID');
+			$stripeID = $stripeID[0];
+			
+			$purchase->setStripeSessionId($stripeID);
+			unset($stripeID);
+		} else {
+			$paypalID = $this->requestStack->getSession()->get('paypalID');
+			$paypalID = $paypalID[0];
+			$purchase->setPaypalOrderId($paypalID);
+			unset($paypalID);
+		}
+		
+		$purchase->setIsPaid(true);
+		
+		$this->em->persist($purchase);
+		$this->em->flush();
+		
+		$products = [];
+		
+		foreach ($cartService->getTotal() as $item) {
+			$product = [
+				'game' => $item['game'],
+				'platform' => $item['platform'],
+				'quantity' => $item['quantity'],
+			];
+			
+			$products[] = $product;
+		}
+		
 		return $this->render('order/success.html.twig', [
 			'reference' => $reference,
+			'products' => $products,
 		]);
 	}
 	
 	//Function to redirect to the error page
 	#[Route('/order/error/{reference}', name: 'payment_error')]
-	public function stripeError($reference, CartService $cartService): Response
+	public function purchaseError($reference, CartService $cartService): Response
 	{
 		return $this->render('order/error.html.twig');
 	}
@@ -138,9 +181,9 @@ class PaymentController extends AbstractController
 			$itemTotal += $product->getPrice() * $product->getQuantity();
 		}
 		
-		$shipping = floatval(number_format($itemTotal / 100 * 20, 2, '.', ''));
-		$total = floatval(number_format($itemTotal + $shipping, 2, '.', ''));
-		$itemTotal = floatval(number_format($itemTotal, 2, '.', ''));
+		$shipping = round($itemTotal / 100 * 20, 2);
+		$total = round($itemTotal + $shipping, 2);
+		$itemTotal = round($itemTotal, 2);
 		
 		$request = new OrdersCreateRequest();
 		$request->prefer('return=representation');
