6320b8413f6edb461e38586bc14bb5351f2bf892
DenZaiyy <denzaiyy@outlook.fr>
2023-05-31 11:59:14 +0200

    up: remove imagetype

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -17,67 +17,62 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
-    public static function getEntityFqcn(): string
-    {
-        return User::class;
-    }
-
-    public function configureCrud(Crud $crud): Crud
-    {
-        return $crud
-            ->setEntityLabelInSingular('User')
-            ->setEntityLabelInPlural('User')
-            ->setSearchFields(['id', 'username', 'roles', 'email', 'avatar', 'is_verified', 'is_banned', 'create_at']);
-    }
-
-    public function configureFilters(Filters $filters): Filters
-    {
-        return $filters
-            ->add('roles')
-            ->add('isVerified')
-	        ->add('isBanned')
+	public static function getEntityFqcn(): string
+	{
+		return User::class;
+	}
+	
+	public function configureCrud(Crud $crud): Crud
+	{
+		return $crud
+			->setEntityLabelInSingular('User')
+			->setEntityLabelInPlural('User')
+			->setSearchFields(['id', 'username', 'roles', 'email', 'avatar', 'is_verified', 'is_banned', 'create_at']);
+	}
+	
+	public function configureFilters(Filters $filters): Filters
+	{
+		return $filters
+			->add('roles')
+			->add('isVerified')
+			->add('isBanned')
 			->add('createAt');
-    }
-
-    public function configureFields(string $pageName): iterable
-    {
-        yield NumberField::new('id')->hideOnForm();
-        yield TextField::new('username');
-        yield ChoiceField::class::new('roles')
-            ->setChoices([
-                'ROLE_USER' => 'ROLE_USER',
-                'ROLE_ADMIN' => 'ROLE_ADMIN',
-                ]
-            )
-            ->allowMultipleChoices();
-        yield TextField::new('email');
-        yield ImageField::new('avatar')
-            ->setBasePath('uploads/avatars')
-            ->setUploadDir('public/uploads/avatars')
-            ->setUploadedFileNamePattern('[randomhash].[extension]')
-            ->setRequired(false);
-
-        $verifiedAcc = BooleanField::new('is_verified')
-	        ->hideOnForm()
-	        ->setFormTypeOption('disabled', true);
-
-        $isBanned =  BooleanField::new('is_banned')->hideOnForm();
-
-        $createdAt = DateTimeField::new('CreateAt')
-            ->setFormat('dd-MM-yyyy')
-            ->setFormTypeOptions([
-                'data' => new DateTime('now', new DateTimeZone('Europe/Paris')), // default data
-                'widget' => 'single_text',
-                'disabled' => true,
-            ]);
-
-        if (Crud::PAGE_EDIT === $pageName) {
-            yield $createdAt->setFormTypeOption('disabled', true);
-            yield $isBanned->setFormTypeOption('disabled', true);
-        } else {
-            yield $createdAt;
-            yield $verifiedAcc;
-            yield $isBanned;
-        }
-    }
+	}
+	
+	public function configureFields(string $pageName): iterable
+	{
+		yield NumberField::new('id')->hideOnForm();
+		yield TextField::new('username');
+		yield ChoiceField::class::new('roles')
+			->setChoices([
+					'ROLE_USER' => 'ROLE_USER',
+					'ROLE_ADMIN' => 'ROLE_ADMIN',
+				]
+			)
+			->allowMultipleChoices();
+		yield TextField::new('email');
+		
+		$verifiedAcc = BooleanField::new('is_verified')
+			->hideOnForm()
+			->setFormTypeOption('disabled', true);
+		
+		$isBanned = BooleanField::new('is_banned')->hideOnForm();
+		
+		$createdAt = DateTimeField::new('CreateAt')
+			->setFormat('dd-MM-yyyy')
+			->setFormTypeOptions([
+				'data' => new DateTime('now', new DateTimeZone('Europe/Paris')), // default data
+				'widget' => 'single_text',
+				'disabled' => true,
+			]);
+		
+		if (Crud::PAGE_EDIT === $pageName) {
+			yield $createdAt->setFormTypeOption('disabled', true);
+			yield $isBanned->setFormTypeOption('disabled', true);
+		} else {
+			yield $createdAt;
+			yield $verifiedAcc;
+			yield $isBanned;
+		}
+	}
 }
