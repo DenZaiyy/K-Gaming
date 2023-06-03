7febbe90b36e7bb7806ce9304adf32dfe4cf895f
DenZaiyy <denzaiyy@outlook.fr>
2023-06-04 01:23:11 +0200

    ADD: knp_paginator for platform page

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
