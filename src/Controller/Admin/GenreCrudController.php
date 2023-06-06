ae8d993201e666354fc188131214bff8fbf29d4d
DenZaiyy <denzaiyy@outlook.fr>
2023-06-06 16:49:41 +0200

    ADD: Genre crud with slug

--- src/Controller/Admin/GenreCrudController.php
+++ src/Controller/Admin/GenreCrudController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Genre;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class GenreCrudController extends AbstractCrudController
 {
@@ -12,14 +18,25 @@ class GenreCrudController extends AbstractCrudController
         return Genre::class;
     }
 
-    /*
+	public function configureCrud(Crud $crud): Crud
+	{
+		return $crud
+			->setEntityLabelInSingular('Genre')
+			->setEntityLabelInPlural('Genre')
+			->setSearchFields(['id', 'label', 'slug']);
+	}
+
+	public function configureFilters(Filters $filters): Filters
+	{
+		return $filters
+			->add('label');
+	}
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield NumberField::new('id')->hideOnForm();
+        yield TextField::new('label');
+        yield SlugField::new('slug')->setTargetFieldName('label');
     }
-    */
+
 }
