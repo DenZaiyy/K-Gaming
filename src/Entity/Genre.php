921052f58151dbea8f1e566e3aed35ca9b554cf3
DenZaiyy <denzaiyy@outlook.fr>
2023-06-07 09:35:58 +0200

    ADD: migration to add column image to genre

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -5,88 +5,104 @@ namespace App\Entity;
 use App\Repository\GenreRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GenreRepository::class)]
 class Genre
 {
 	#[ORM\Id]
-	#[ORM\GeneratedValue]
-	#[ORM\Column]
-	private ?int $id = null;
+            	#[ORM\GeneratedValue]
+            	#[ORM\Column]
+            	private ?int $id = null;
 
 	#[ORM\Column(length: 50)]
-	private ?string $label = null;
+            	private ?string $label = null;
 
 	#[ORM\ManyToMany(targetEntity: Game::class, mappedBy: 'genres')]
-	private Collection $games;
+            	private Collection $games;
 
 	#[ORM\Column(length: 255)]
-	private ?string $slug = null;
+            	private ?string $slug = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $image = null;
 
 	public function __toString(): string
-	{
-		return $this->label;
-	}
+            	{
+            		return $this->label;
+            	}
 
 	public function __construct()
-	{
-		$this->games = new ArrayCollection();
-	}
+            	{
+            		$this->games = new ArrayCollection();
+            	}
 
 	public function getId(): ?int
-	{
-		return $this->id;
-	}
+            	{
+            		return $this->id;
+            	}
 
 	public function getLabel(): ?string
-	{
-		return $this->label;
-	}
+            	{
+            		return $this->label;
+            	}
 
 	public function setLabel(string $label): self
-	{
-		$this->label = $label;
-
-		return $this;
-	}
+            	{
+            		$this->label = $label;
+            
+            		return $this;
+            	}
 
 	/**
 	 * @return Collection<int, Game>
 	 */
 	public function getGames(): Collection
-	{
-		return $this->games;
-	}
+            	{
+            		return $this->games;
+            	}
 
 	public function addGame(Game $game): self
-	{
-		if (!$this->games->contains($game)) {
-			$this->games->add($game);
-			$game->addGenre($this);
-		}
-
-		return $this;
-	}
+            	{
+            		if (!$this->games->contains($game)) {
+            			$this->games->add($game);
+            			$game->addGenre($this);
+            		}
+            
+            		return $this;
+            	}
 
 	public function removeGame(Game $game): self
-	{
-		if ($this->games->removeElement($game)) {
-			$game->removeGenre($this);
-		}
-
-		return $this;
-	}
+            	{
+            		if ($this->games->removeElement($game)) {
+            			$game->removeGenre($this);
+            		}
+            
+            		return $this;
+            	}
 
 	public function getSlug(): ?string
-	{
-		return $this->slug;
-	}
+            	{
+            		return $this->slug;
+            	}
 
 	public function setSlug(string $slug): self
-	{
-		$this->slug = $slug;
+            	{
+            		$this->slug = $slug;
+            
+            		return $this;
+            	}
 
-		return $this;
-	}
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
 }
