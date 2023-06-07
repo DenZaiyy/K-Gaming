dba43e887e79c685082b3c626d0c120ea49b4ac8
DenZaiyy <denzaiyy@outlook.fr>
2023-06-07 17:30:39 +0200

    ADD: rating file for rating system

---
+++ assets/rating.js
@@ -0,0 +1,53 @@
+window.onload = () => {
+    const ratingStar = document.querySelector('.ratingStars')
+    console.log(ratingStar.children)
+    const rating = document.getElementById('rating_note')
+    const label = document.getElementById('labelRating')
+
+    for (starsTest of ratingStar.children) {
+        starsTest.addEventListener('mouseover', function() {
+            resetStars();
+            this.style.color = "#f44b3a";
+            this.classList.add('bi-star-fill');
+            this.classList.remove('bi-star');
+
+            label.innerHTML = this.dataset.label;
+
+            let previousStar = this.previousElementSibling;
+
+            while(previousStar)
+            {
+                previousStar.style.color = "#f44b3a";
+                previousStar.classList.add('bi-star-fill');
+                previousStar.classList.remove('bi-star');
+                previousStar = previousStar.previousElementSibling;
+            }
+        })
+
+        starsTest.addEventListener('click', function() {
+            rating.value = this.dataset.value;
+            label.innerHTML = this.dataset.label;
+        })
+
+        starsTest.addEventListener('mouseout', function() {
+            label.innerHTML = ""
+            resetStars(rating.value);
+        })
+    }
+    function resetStars(nb = 0)
+    {
+        for(starsTest of ratingStar.children)
+        {
+            if (starsTest.dataset.value > nb) {
+                starsTest.style.color = "black";
+                starsTest.classList.add('bi-star');
+                starsTest.classList.remove('bi-star-fill');
+            } else {
+                starsTest.style.color = "#f44b3a";
+                starsTest.classList.add('bi-star-fill');
+                starsTest.classList.remove('bi-star');
+                label.innerHTML = starsTest.dataset.label;
+            }
+        }
+    }
+}
