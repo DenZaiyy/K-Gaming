6b53b87982cddecd336b5906c05e4372e3f72f47
DenZaiyy <denzaiyy@outlook.fr>
2023-06-08 16:48:13 +0200

    ADD: new migrations

---
+++ migrations/Version20230608144011.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230608144011 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recap_details ADD game_slug VARCHAR(255) NOT NULL, ADD platform_slug VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recap_details DROP game_slug, DROP platform_slug');
+    }
+}
