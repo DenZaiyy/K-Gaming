4acbb8c0818409c0ddc9960a18bf778478fc7916
DenZaiyy <denzaiyy@outlook.fr>
2023-06-09 15:29:50 +0200

    UP: check if ratingStar exists before execute

--- assets/rating.js
+++ assets/rating.js
@@ -1,43 +1,42 @@
 window.onload = () => {
     const ratingStar = document.querySelector('.ratingStars')
-    console.log(ratingStar.children)
     const rating = document.getElementById('rating_note')
     const label = document.getElementById('labelRating')
 
-    for (starsTest of ratingStar.children) {
-        starsTest.addEventListener('mouseover', function() {
-            resetStars();
-            this.style.color = "#f44b3a";
-            this.classList.add('bi-star-fill');
-            this.classList.remove('bi-star');
+    if (ratingStar) {
+        for (starsTest of ratingStar.children) {
+            starsTest.addEventListener('mouseover', function () {
+                resetStars();
+                this.style.color = "#f44b3a";
+                this.classList.add('bi-star-fill');
+                this.classList.remove('bi-star');
 
-            label.innerHTML = this.dataset.label;
+                label.innerHTML = this.dataset.label;
 
-            let previousStar = this.previousElementSibling;
+                let previousStar = this.previousElementSibling;
 
-            while(previousStar)
-            {
-                previousStar.style.color = "#f44b3a";
-                previousStar.classList.add('bi-star-fill');
-                previousStar.classList.remove('bi-star');
-                previousStar = previousStar.previousElementSibling;
-            }
-        })
+                while (previousStar) {
+                    previousStar.style.color = "#f44b3a";
+                    previousStar.classList.add('bi-star-fill');
+                    previousStar.classList.remove('bi-star');
+                    previousStar = previousStar.previousElementSibling;
+                }
+            })
 
-        starsTest.addEventListener('click', function() {
-            rating.value = this.dataset.value;
-            label.innerHTML = this.dataset.label;
-        })
+            starsTest.addEventListener('click', function () {
+                rating.value = this.dataset.value;
+                label.innerHTML = this.dataset.label;
+            })
 
-        starsTest.addEventListener('mouseout', function() {
-            label.innerHTML = ""
-            resetStars(rating.value);
-        })
+            starsTest.addEventListener('mouseout', function () {
+                label.innerHTML = ""
+                resetStars(rating.value);
+            })
+        }
     }
-    function resetStars(nb = 0)
-    {
-        for(starsTest of ratingStar.children)
-        {
+
+    function resetStars(nb = 0) {
+        for (starsTest of ratingStar.children) {
             if (starsTest.dataset.value > nb) {
                 starsTest.style.color = "black";
                 starsTest.classList.add('bi-star');
