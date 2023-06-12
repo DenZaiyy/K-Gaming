d82d619e1cef5051424441aad45304006f17b645
DenZaiyy <denzaiyy@outlook.fr>
2023-06-12 17:10:12 +0200

    ADD: searchData Form

---
+++ src/Form/SearchForm.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form;
+
+use App\Data\SearchData;
+use App\Entity\Genre;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchForm extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('q', TextType::class, [
+				'label' => false,
+				'required' => false,
+				'attr' => [
+					'placeholder' => 'Rechercher'
+				]
+			])
+			->add('genres', EntityType::class, [
+				'label' => false,
+				'required' => false,
+				'class' => Genre::class,
+				'expanded' => true,
+				'multiple' => true
+			])
+			->add('min', NumberType::class, [
+				'label' => false,
+				'required' => false,
+				'attr' => [
+					'placeholder' => 'Prix min'
+				]
+			])
+			->add('max', NumberType::class, [
+				'label' => false,
+				'required' => false,
+				'attr' => [
+					'placeholder' => 'Prix max'
+				]
+			])
+			->add('preorder', CheckboxType::class, [
+				'label' => 'En précommande',
+				'required' => false
+			])
+		;
+	}
+
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults([
+			'data_class' => SearchData::class,
+			'method' => 'GET',
+			'csrf_protection' => false
+		]);
+	}
+
+	public function getBlockPrefix(): string
+	{
+		return '';
+	}
+}
