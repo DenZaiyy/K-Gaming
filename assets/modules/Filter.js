f7b26b2beab08bd87ebbf0787c2aff9b322c9720
DenZaiyy <denzaiyy@outlook.fr>
2023-06-13 14:22:57 +0200

    UP: add loader & animate content

--- assets/modules/Filter.js
+++ assets/modules/Filter.js
@@ -5,6 +5,8 @@ import {Flipper, spring} from "flip-toolkit"
  * @property {HTMLElement} content
  * @property {HTMLElement} sorting
  * @property {HTMLFormElement} form
+ * @property {number} page
+ * @property {boolean} moreNav
  */
 export default class Filter {
 
@@ -19,6 +21,10 @@ export default class Filter {
         this.content = element.querySelector('.js-filter-content')
         this.sorting = element.querySelector('.js-filter-sorting')
         this.form = element.querySelector('.js-filter-form')
+
+        this.page = parseInt(new URLSearchParams(window.location.search).get('page') || 1)
+        this.moreNav = this.page === 1
+
         this.bindEvents()
         console.log("test")
     }
@@ -34,14 +40,34 @@ export default class Filter {
             }
         }
 
-        this.sorting.addEventListener('click', linkClickListeners)
-        this.pagination.addEventListener('click', linkClickListeners)
+        this.sorting.addEventListener('click', e => {
+            linkClickListeners(e)
+            this.page = 1
+        })
+        if(this.moreNav) {
+            this.pagination.innerHTML = '<button class ="btn btn-primary">Voir plus</button>'
+            this.pagination.querySelector('button').addEventListener('click', this.loadMore.bind(this))
+        } else {
+            this.pagination.addEventListener('click', linkClickListeners)
+        }
         this.form.querySelectorAll('input').forEach(input => {
             input.addEventListener('change', this.loadForm.bind(this))
         })
     }
 
+    async loadMore() {
+        const button = this.pagination.querySelector('button')
+        button.setAttribute('disabled', 'disabled')
+        this.page++
+        const url = new URL(window.location.href)
+        const params = new URLSearchParams(url.search)
+        params.set('page', this.page)
+        await this.loadUrl(url.pathname + '?' + params.toString(), true)
+        button.removeAttribute('disabled')
+    }
+
     async loadForm() {
+        this.page = 1
         const data = new FormData(this.form)
         const url = new URL(this.form.getAttribute('action') || window.location.href)
         const params = new URLSearchParams() //construit les params d'une url dynamiquement
@@ -53,32 +79,47 @@ export default class Filter {
         return this.loadUrl(url.pathname + '?' + params.toString())
     }
 
-    async loadUrl(url) {
-        const ajaxUrl = url + '&ajax=1'
-        const response = await fetch(ajaxUrl, {
+    async loadUrl(url, append = false) {
+        this.showLoader()
+
+        const params = new URLSearchParams(url.split('?')[1] || '')
+        params.set('ajax', 1)
+
+        const response = await fetch(url.split('?')[0] + '?' + params.toString(), {
             headers: {
                 'X-Requested-With': 'XMLHttpRequest'
             }
         })
+
         if (response.status >= 200 && response.status < 300) {
             const data = await response.json()
-            this.flipContent(data.content)
+            this.flipContent(data.content, append)
             this.sorting.innerHTML = data.sorting
-            this.pagination.innerHTML = data.pagination
-            history.replaceState({}, '', url)
+            if(!this.moreNav) {
+                this.pagination.innerHTML = data.pagination
+            } else if (this.page === data.pages) {
+                this.pagination.style.display = "none";
+            } else {
+                this.pagination.style.display = null;
+            }
+            this.updatePrices(data)
+            params.delete('ajax')
+            history.replaceState({}, '', url.split('?')[0] + '?' + params.toString())
         } else {
             console.error(response)
         }
+        this.hideLoader()
     }
 
     /**
      * Remplace les éléments de la grille avec un effet d'animation flip
      * @param {string} content
      */
-    flipContent(content) {
+    flipContent(content, append = false) {
+        const springConfig = 'gentle'
         const exitSpring = function (element, index, onComplete) {
             spring({
-                config: "wobbly",
+                config: "stiff",
                 values: {
                     translateY: [0, -20],
                     opacity: [1, 0]
@@ -90,6 +131,20 @@ export default class Filter {
                 onComplete
             })
         }
+        const appearSpring = function (element, index) {
+            spring({
+                config: "stiff",
+                values: {
+                    translateY: [20, 0],
+                    opacity: [0, 1]
+                },
+                onUpdate: ({translateY, opacity}) => {
+                    element.style.opacity = opacity;
+                    element.style.transform = `translateY(${translateY}px)`;
+                },
+                delay: index * 15
+            })
+        }
 
         const flipper = new Flipper({
             element: this.content
@@ -99,6 +154,7 @@ export default class Filter {
         Array.prototype.forEach.call(this.content.children, (element => {
             flipper.addFlipped({
                 element,
+                spring: springConfig,
                 flipId: element.id,
                 shouldFlip: false,
                 onExit: exitSpring
@@ -106,7 +162,11 @@ export default class Filter {
         }))
         flipper.recordBeforeUpdate()
 
-        this.content.innerHTML = content
+        if(append) {
+            this.content.innerHTML += content
+        } else {
+            this.content.innerHTML = content
+        }
 
         let test = [...this.content.children]
 
@@ -114,9 +174,50 @@ export default class Filter {
         test.forEach(element => {
             flipper.addFlipped({
                 element,
-                flipId: element.id
+                spring: springConfig,
+                flipId: element.id,
+                onAppear: appearSpring
             })
         })
         flipper.update()
+    }
+
+    showLoader() {
+        this.form.classList.add('is-loading');
+        const loader = this.form.querySelector('.js-loading')
+
+        if(loader === null) {
+            return
+        }
+
+        loader.setAttribute('aria-hidden', 'false')
+        loader.style.display = null
+    }
+
+    hideLoader() {
+        this.form.classList.remove('is-loading');
+        const loader = this.form.querySelector('.js-loading')
+
+        if(loader === null) {
+            return
+        }
+
+        loader.setAttribute('aria-hidden', 'true')
+        loader.style.display = "none"
+    }
+
+    updatePrices({min, max}) {
+        const slider = document.getElementById('price-slider')
+
+        if(slider === null) {
+            return
+        }
+
+        slider.noUiSlider.updateOptions({
+            range: {
+                min: [min],
+                max: [max],
+            }
+        })
     }
 }
