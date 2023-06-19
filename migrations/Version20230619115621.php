f83f7d00a1b9b2288ef25ec860bd7f155c2c63ef
DenZaiyy <denzaiyy@outlook.fr>
2023-06-19 15:30:33 +0200

    ADD: migrations

---
+++ migrations/Version20230619115621.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230619115621 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE facture (id INT AUTO_INCREMENT NOT NULL, purchase_id INT NOT NULL, reference VARCHAR(255) NOT NULL, facture LONGTEXT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_FE866410558FBEB9 (purchase_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE facture ADD CONSTRAINT FK_FE866410558FBEB9 FOREIGN KEY (purchase_id) REFERENCES purchase (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE facture DROP FOREIGN KEY FK_FE866410558FBEB9');
+        $this->addSql('DROP TABLE facture');
+    }
+}
