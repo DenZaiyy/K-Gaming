1cb162668ff137725cbf287530f5559b99ce7bc7
DenZaiyy <denzaiyy@outlook.fr>
2023-06-22 17:39:30 +0200

    ADD: service for multiavatar api

---
+++ src/Service/MultiAvatars.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class MultiAvatars extends AbstractController
+{
+    public function __construct(private readonly HttpClientInterface $client)
+    {
+    }
+
+    public function api($imageID)
+    {
+        $response = $this->client->request(
+            'GET',
+            'https://api.multiavatar.com/' . $imageID . '.png?apikey=oE7Qgxe34yjiMh',
+        );
+
+        return $response->getInfo('url');
+    }
+
+    public function getRandomUserAvatar($user): string
+    {
+        return $this->api($user . '-' . rand(1, 1000));
+    }
+
+    public function getRandomAvatar(): string
+    {
+        return rand(1, 1000);
+    }
+
+    public function getFiveRandomlyAvatar(): array
+    {
+        for ($i = 0; $i < 8; $i++) {
+            $avatars[] = $this->api($this->getRandomAvatar());
+        }
+        return $avatars;
+    }
+
+    public function setAvatarUrl($imageID)
+    {
+        return $this->api($imageID);
+    }
+}
