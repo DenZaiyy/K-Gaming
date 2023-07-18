ce9b82bbaf5aa454ffd8d944517acb8d6489cec1
DenZaiyy <denzaiyy@outlook.fr>
2023-07-18 18:19:24 +0200

    ADD: unique on email for user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -32,7 +32,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
