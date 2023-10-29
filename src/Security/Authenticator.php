befbe36a882e4b851366819db1ac0f80a4979e90
DenZaiyy <denzaiyy@outlook.fr>
2023-10-29 11:46:19 +0100

    Update Authenticator.php

--- src/Security/Authenticator.php
+++ src/Security/Authenticator.php
@@ -73,7 +73,7 @@ class Authenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         return new RedirectResponse($this->urlGenerator->generate('app_home'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
