ee0be9867a17a41c757cdd43000e178ca9c42bbe
DenZaiyy <denzaiyy@outlook.fr>
2023-11-25 17:45:53 +0100

    add: promotion filtre for twig to have good look

---
+++ src/Twig/promoFilter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class promoFilter extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('promo', [$this, 'promo'])
+        ];
+
+    }
+
+    public function promo($percentage, string $symbol = '%'): string
+    {
+        return '-' . $percentage . $symbol;
+    }
+}
