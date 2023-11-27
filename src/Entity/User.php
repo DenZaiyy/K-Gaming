3b67b50fa9d54bc9d588aae669f8e82aecb4a767
DenZaiyy <denzaiyy@outlook.fr>
2023-11-27 15:18:56 +0100

    :+1:  toString function to User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -298,4 +298,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+	public function __tosTring(): string
+	{
+		return $this->username;
+	}
 }
