1070640ff50701405116478b3eb596506f049a30
DenZaiyy <denzaiyy@outlook.fr>
2023-12-18 21:50:34 +0100

    feat: :hammer: add DateFormatter with translations for twig filter

---
+++ src/Twig/DateFormatter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Twig;
+
+use DateTime;
+use DateTimeImmutable;
+use IntlDateFormatter;
+use Twig\TwigFunction;
+use Twig\Extension\AbstractExtension;
+
+class DateFormatter extends AbstractExtension
+{
+	//	getFunctions permet de récupérer dans le fichier twig le nom de la fonction et la fonction à appeler (dateFR)
+
+	public function getFunctions(): array
+	{
+		return [
+			new TwigFunction('dateTranslated', [$this, 'dateTranslated']),
+            new TwigFunction('fullDateTranslated', [$this, 'fullDateTranslated'])
+		];
+	}
+
+	// fonction permettant de formater la date en français et en toute lettre
+	public function dateTranslated(DateTime $date, string $lang): string
+	{
+		if($lang == 'fr') {
+			$formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+		} else if ($lang == 'en') {
+			$formatter = new IntlDateFormatter('en_EN', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+		} else {
+			$formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+		}
+
+		return $formatter->format($date);
+	}
+
+	/*
+	 * fonction permettant de formater la date en français et en toute lettre
+	 */
+    public function fullDateTranslated(DateTimeImmutable $date, string $lang): string
+    {
+	    if($lang == "fr") {
+		    $formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::SHORT);
+	    } else if ($lang == "en"){
+		    $formatter = new IntlDateFormatter('en_EN', IntlDateFormatter::LONG, IntlDateFormatter::SHORT);
+	    } else {
+			$formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::SHORT);
+	    }
+
+        return $formatter->format($date);
+    }
+}
