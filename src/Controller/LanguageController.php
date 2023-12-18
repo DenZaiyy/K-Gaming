83ab1b1296464cca9f8cbe9082d3bde886cc5012
DenZaiyy <denzaiyy@outlook.fr>
2023-12-18 21:46:26 +0100

    feat: :adhesive_bandage: add locale switcher with route

---
+++ src/Controller/LanguageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LanguageController extends AbstractController
+{
+	#[Route('/change-language/{_locale}', name: 'change_language')]
+	public function index (Request $request): Response
+	{
+		$locale = $request->attributes->get('_locale');
+		$referer = $request->headers->get('referer');
+
+		// Check if the referer URL contains a locale (e.g., 'en' or 'fr')
+		if (preg_match('/\/(en|fr)\//', $referer, $matches)) {
+			$newReferer = preg_replace('/\/(en|fr)\//', '/' . $locale . '/', $referer, 1);
+			$redirect = $this->redirect($newReferer);
+			$redirect->headers->setCookie(new Cookie('_locale', $locale));
+			return $redirect;
+		}
+
+		$response = $this->redirectToRoute('app_home', ['_locale' => $locale]);
+		$response->headers->setCookie(new Cookie('_locale', $locale));
+		return $response;
+	}
+}
