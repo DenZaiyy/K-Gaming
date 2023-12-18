1811194b8bf78a59039a3d1ff6f5d765272a8bb1
DenZaiyy <denzaiyy@outlook.fr>
2023-12-18 21:48:14 +0100

    fix: :adhesive_bandage: get cookie for locale and update form newsletter in homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Controller\Newsletter\SubscriptionController;
 use App\Entity\Category;
 use App\Entity\Game;
 use App\Entity\Genre;
@@ -10,6 +11,7 @@ use App\Entity\Stock;
 use App\Service\CartService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -21,7 +23,16 @@ class HomeController extends AbstractController
 	#[Route('/')]
 	public function indexNoLocale(Request $request): Response
 	{
-	    return $this->redirectToRoute('app_home', ['_locale' => $request->getLocale()]);
+		if (!$request->cookies->get('_locale')) {
+			$locale = $request->getDefaultLocale();
+		} else {
+			$locale = $request->cookies->get('_locale');
+		}
+
+		$response = $this->redirectToRoute('app_home', ['_locale' => $locale]);
+		$response->headers->setCookie(new Cookie('_locale', $request->getLocale()));
+		$response->send();
+	    return $response;
 	}
 	/*
 	 * Méthode permettant d'afficher la page d'accueil du site avec les informations nécessaires tel que les jeux en tendances, les précommandes et les genres
@@ -44,10 +55,16 @@ class HomeController extends AbstractController
 		$preorders = $em->getRepository(Game::class)->findGamesInPreOrders(3);
 		$genres = $em->getRepository(Genre::class)->findGenres($resultPerPage);
 
+		$newsletter = $this->forward(SubscriptionController::class . 'subscribe', [
+			'request' => $request
+		]);
+//		dd($newsletter);
+
 		return $this->render('home/index.html.twig', [
 			'tendencies' => $tendencies,
 			'preorders' => $preorders,
 			'genres' => $genres,
+			'newsletter' => $newsletter,
             'description' => "Bienvenue sur le site K-GAMING, le site de vente de jeux vidéo en ligne. Retrouvez les dernières nouveautés et les meilleurs jeux du moment."
 		]);
 	}
