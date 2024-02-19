2dcbdd830f313696145d09b0b976a48b412245a1
DenZaiyy <denzaiyy@outlook.fr>
2024-02-19 19:27:02 +0100

    feat: upgrade to sf7 and remove all deprecations

--- src/Form/SearchForm.php
+++ src/Form/SearchForm.php
@@ -14,7 +14,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SearchForm extends AbstractType
 {
-	public function buildForm(FormBuilderInterface $builder, array $options)
+	public function buildForm(FormBuilderInterface $builder, array $options): void
 	{
 		$builder
 			->add('q', TextType::class, [
@@ -56,7 +56,7 @@ class SearchForm extends AbstractType
 		;
 	}
 
-	public function configureOptions(OptionsResolver $resolver)
+	public function configureOptions(OptionsResolver $resolver): void
 	{
 		$resolver->setDefaults([
 			'data_class' => SearchData::class,
