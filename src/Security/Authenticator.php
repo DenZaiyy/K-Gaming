7cf03ce5c5bbe4a99ff124c1046bd998c4d83a3c
DenZaiyy <denzaiyy@outlook.fr>
2024-02-19 19:57:23 +0100

    fix: Authenticator.php

--- src/Security/Authenticator.php
+++ src/Security/Authenticator.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
@@ -37,6 +36,11 @@ class Authenticator extends AbstractLoginFormAuthenticator
 
 		$user = $this->userRepository->findOneBy(['username' => $username]);
 
+		if(!$user)
+		{
+			throw new CustomUserMessageAuthenticationException('Nom d\'utilisateur ou mot de passe incorrect.');
+		}
+
 		if(!$user->isVerified())
 		{
 			throw new CustomUserMessageAuthenticationException('Veuillez vérifier votre adresse mail avant de vous connecter.');
@@ -46,9 +50,6 @@ class Authenticator extends AbstractLoginFormAuthenticator
         if(!empty($honeypot)) {
             throw new CustomUserMessageAuthenticationException("Le formulaire n'a pas été soumis correctement.");
         }
-
-        // On stocke le nom d'utilisateur dans la session pour le réafficher dans le formulaire en cas d'erreur
-        $request->getSession()->set(Security::LAST_USERNAME, $username);
 
         // On retourne un objet Passport qui contient les informations de connexion
         return new Passport(
