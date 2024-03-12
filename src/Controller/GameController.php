49d04b89a6b4d3a776e3756448eae5fd2d0d2763
DenZaiyy <denzaiyy@outlook.fr>
2024-03-12 02:16:55 +0100

    feat: api refactor

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -23,7 +23,7 @@ class GameController extends AbstractController
      * Constructeur permettant d'instancier le service "CallApiService" et permettant de l'utiliser dans les méthodes de la classe
      * @param CallApiService $callApiService
      */
-    public function __construct (private readonly CallApiService $callApiService)
+    public function __construct(private readonly CallApiService $callApiService)
     {
     }
 
@@ -31,39 +31,43 @@ class GameController extends AbstractController
      * Méthode permettant d'afficher le detail d'un jeu grâce au slug
      */
     #[Route("/{_locale<%app.supported_locales%>}/{gameSlug}", name: "app_show_game", priority: -1)]
-    public function showGame (EntityManagerInterface $em, $gameSlug, BreadCrumbsService $breadCrumbsService): Response
+    public function showGame(EntityManagerInterface $em, $gameSlug, BreadCrumbsService $breadCrumbsService): Response
     {
         $game = $em->getRepository(Game::class)->findOneBy(["slug" => $gameSlug]);
+
         if (!$game) {
             $this->addFlash("danger", 'Le jeu n\'existe pas');
             return $this->redirectToRoute("app_404");
         }
-        $gameStock = $em->getRepository(Stock::class)->findStockByGameID($game->getId());
-        $gamePlatform = $em->getRepository(Game::class)->findOneGameInPlatform(
-          $game->getId(), $gameStock[0]["platform_id"]
-        );
-        $platform = $em->getRepository(Plateform::class)->findOneBy(["id" => $gamePlatform["platform_id"]]);
 
+        $gameStock = $em->getRepository(Stock::class)->findStockByGameID($game->getId());
+        $gamePlatform = $em->getRepository(Game::class)->findOneGameInPlatform($game->getId(), $gameStock[0]["platform_id"]);
+        $platform = $em->getRepository(Plateform::class)->findOneBy(["id" => $gamePlatform["platform_id"]]);
+        if (!$platform) {
+            $this->addFlash("danger", 'La plateforme n\'existe pas');
+            return $this->redirectToRoute("app_404");
+        }
         $ratings = $em->getRepository(Rating::class)->findBy(["game" => $game->getId()]);
 
-        // TODO: Refactor this
-        /*$breadcrumbs->addRouteItem($game->getLabel(), "app_show_game", ['gameSlug' => $gameSlug]);
-            $breadcrumbs->prependRouteItem($gamePlatform['platform_label'], "platform_game", ['categoryLabel' => $platform->getCategory()->getLabel(),'platformSlug' => $gamePlatform['platform_slug']]);
-            $breadcrumbs->prependRouteItem($platform->getCategory()->getLabel(), "platform_categories", ['categoryLabel' => $platform->getCategory()->getLabel()]);
-            $breadcrumbs->prependRouteItem("Accueil", "app_home");*/
-
         //TODO: This is a temporary solution until we can refactor the breadcrumbs service
-        $breadCrumbsService->BCGenerate(["label" => $platform->getCategory()->getLabel(),
-          "route" => "platform_categories",
-          "params" => ["categoryLabel" => strtolower($platform->getCategory()->getLabel()),],],
-          ["label" => $gamePlatform["platform_label"],
+        $breadCrumbsService->BCGenerate([
+            "label" => $platform->getCategory()->getLabel(),
+            "route" => "platform_categories",
+            "params" => ["categoryLabel" => strtolower($platform->getCategory()->getLabel())]
+        ], [
+            "label" => $gamePlatform["platform_label"],
             "route" => "platform_game",
-            "params" => ["categoryLabel" => $platform->getCategory()->getLabel(),
-              "platformSlug" => $gamePlatform["platform_slug"],],], ["label" => $game->getLabel(),
+            "params" => [
+                "categoryLabel" => $platform->getCategory()->getLabel(),
+                "platformSlug" => $gamePlatform["platform_slug"]
+            ]
+        ], [
+            "label" => $game->getLabel(),
             "route" => "app_show_game",
-            "params" => ["gameSlug" => $gameSlug],], []);
+            "params" => ["gameSlug" => $gameSlug]
+        ], []);
 
-        $moyenne = 0;
+        $average = 0;
         // Verification si le jeu a des notes pour calculer la moyenne
         if ($ratings) {
             $somme = 0;
@@ -71,54 +75,51 @@ class GameController extends AbstractController
                 $somme += $rate->getNote();
             }
 
-            $moyenne = $somme / count($ratings);
+            $average = $somme / count($ratings);
         }
 
         return $this->render("game/show.html.twig", ["game" => $game,
-          "gameStock" => $gameStock[0],
-          "gamePlatform" => $gamePlatform,
-          "ratings" => $ratings,
-          "moyenne" => $moyenne,
-          "category" => strtolower($platform->getCategory()->getLabel()),
-          "description" => "Retrouvez toutes les informations concernant le jeu " . $game->getLabel(
-            ) . " sur K-Gaming.",]);
+            "gameStock" => $gameStock[0],
+            "gamePlatform" => $gamePlatform,
+            "ratings" => $ratings,
+            "average" => $average,
+            "category" => strtolower($platform->getCategory()->getLabel()),
+            "description" => "Retrouvez toutes les informations concernant le jeu " . $game->getLabel() . " sur K-Gaming.",]);
     }
 
     /*
      * Méthode permettant d'afficher le detail d'un jeu dans une plateforme grâce aux slugs
      */
     #[Route("/{_locale<%app.supported_locales%>}/platform/{categoryLabel}/{platformSlug}/{gameSlug}", name: "app_show_game_platform")]
-    public function showGameInPlatform (
-      EntityManagerInterface $em,
-      $platformSlug,
-      $gameSlug,
-      BreadCrumbsService $breadCrumbsService
-    ): Response {
-        $game = $em->getRepository(Game::class)->findOneBy(["slug" => $gameSlug]);
+    public function showGameInPlatform(EntityManagerInterface $em, $platformSlug, $gameSlug, BreadCrumbsService $breadCrumbsService): Response
+    {
+//        $game = $em->getRepository(Game::class)->findOneBy(["slug" => $gameSlug]);
+        $game = $this->callApiService->getInfosByGames(["gameSlug_Platform" => $gameSlug]);
+//        dd($game);
+        if (!$game) {
+            $this->addFlash("danger", 'Le jeu n\'existe pas');
+            return $this->redirectToRoute("app_404");
+        }
         $platform = $em->getRepository(Plateform::class)->findOneBy(["slug" => $platformSlug]);
-        $gamePlatform = $em->getRepository(Game::class)->findOneGameInPlatform($game->getId(), $platform->getId());
-        $gameStock = $em->getRepository(Stock::class)->findAvailableGameStockByPlatform(
-          $game->getId(), $platform->getId()
-        );
+        if (!$platform) {
+            $this->addFlash("danger", 'La plateforme n\'existe pas');
+            return $this->redirectToRoute("app_404");
+        }
+        $gamePlatform = $em->getRepository(Game::class)->findOneGameInPlatform($game[0]['game']->getId(), $platform->getId());
+        $gameStock = $em->getRepository(Stock::class)->findAvailableGameStockByPlatform($game[0]['game']->getId(), $platform->getId());
 
-        $ratings = $em->getRepository(Rating::class)->findBy(["game" => $game->getId()]);
-
-        // TODO: Refactor this
-        /*$breadcrumbs->addRouteItem($game->getLabel(), "app_show_game", ['gameSlug' => $gameSlug]);
-            $breadcrumbs->prependRouteItem($gamePlatform['platform_label'], "platform_game", ['platformSlug' => $gamePlatform['platform_slug'], 'categoryLabel' => $platform->getCategory()->getLabel()]);
-            $breadcrumbs->prependRouteItem($platform->getCategory()->getLabel(), "platform_categories", ['categoryLabel' => $platform->getCategory()->getLabel()]);
-            $breadcrumbs->prependRouteItem("Accueil", "app_home");*/
+        $ratings = $em->getRepository(Rating::class)->findBy(["game" => $game[0]['game']->getId()]);
 
         //TODO: This is a temporary solution until we can refactor the breadcrumbs service
         $breadCrumbsService->BCGenerate(["label" => $platform->getCategory()->getLabel(),
-          "route" => "platform_categories",
-          "params" => ["categoryLabel" => $platform->getCategory()->getLabel()],],
-          ["label" => $gamePlatform["platform_label"],
-            "route" => "platform_game",
-            "params" => ["platformSlug" => $gamePlatform["platform_slug"],
-              "categoryLabel" => $platform->getCategory()->getLabel(),],], ["label" => $game->getLabel(),
-            "route" => "app_show_game",
-            "params" => ["gameSlug" => $gameSlug],], []);
+            "route" => "platform_categories",
+            "params" => ["categoryLabel" => $platform->getCategory()->getLabel()],],
+            ["label" => $gamePlatform["platform_label"],
+                "route" => "platform_game",
+                "params" => ["platformSlug" => $gamePlatform["platform_slug"],
+                    "categoryLabel" => $platform->getCategory()->getLabel(),],], ["label" => $game[0]['game']->getLabel(),
+                "route" => "app_show_game",
+                "params" => ["gameSlug" => $gameSlug],], []);
 
         $moyenne = 0;
         // Verification si le jeu a des notes pour calculer la moyenne
@@ -131,127 +132,75 @@ class GameController extends AbstractController
             $moyenne = $somme / count($ratings);
         }
 
-        return $this->render("game/show.html.twig", ["game" => $game,
-          "gamePlatform" => $gamePlatform,
-          "gameStock" => $gameStock,
-          "ratings" => $ratings,
-          "moyenne" => $moyenne,
-          "category" => strtolower($platform->getCategory()->getLabel()),
-          "description" => "Retrouvez toutes les informations concernant le jeu " . $game->getLabel(
-            ) . " sur K-Gaming.",]);
+        return $this->render("game/show.html.twig", [
+            "game" => $game,
+            "gamePlatform" => $gamePlatform,
+            "gameStock" => $gameStock,
+            "ratings" => $ratings,
+            "moyenne" => $moyenne,
+            "category" => strtolower($platform->getCategory()->getLabel()),
+            "description" => "Retrouvez toutes les informations concernant le jeu " . $game[0]['game']->getLabel() . " sur K-Gaming."
+        ]);
     }
 
     /*
      * Méthode permettant d'afficher la liste des jeux en précommande
      */
-    #[Route(path: ["fr" => "/fr/jeux/precommande", "en" => "/en/preorder/game",], name: "app_show_preorders")]
-    public function showGameInPreorder (EntityManagerInterface $em): Response
+    #[Route("/{_locale<%app.supported_locales%>}/preorder/games", name: "app_show_preorders")]
+    public function showGameInPreorder(): Response
     {
         $date = new DateTime(); // Date du jour
-        $gamePreorder = $em->getRepository(Game::class)->findGameInPreorder(
-          $date
-        ); // Jeux en précommande (date de sortie > date du jour)
+        $gamePreorder = $this->callApiService->getInfosByGames(["preorders" => $date]);
 
-        return $this->render("game/preOrder/index.html.twig", ["gamePreorder" => $gamePreorder,
-          "description" => "Retrouvez toutes les informations concernant les jeux en précommande sur K-Gaming.",]);
+        return $this->render("game/preOrder/index.html.twig", [
+            "gamePreorder" => $gamePreorder,
+            "description" => "Retrouvez toutes les informations concernant les jeux en précommande sur K-Gaming."
+        ]);
     }
 
     /*
      * Méthode permettant d'afficher la liste des jeux associés à un genre grâce au slug
      */
-    #[Route("/{_locale<%app.supported_locales%>}/game/genre/{genreSlug}", name: "app_show_game_genre")]
-    public function showGameByGenre (
-      EntityManagerInterface $em,
-      Request $request,
-      PaginatorInterface $paginator,
-      $genreSlug,
-      BreadCrumbsService $breadCrumbsService,
-	    CallApiService $callApiService
-    ): Response {
-        $gamesDB = $em->getRepository(Game::class)->findGameByGenre(
-          $genreSlug
-        ); // Récupérer la liste des jeux associés à un genre
+    #[Route("/{_locale<%app.supported_locales%>}/game/gender/{genreSlug}", name: "app_show_game_genre")]
+    public function showGameByGenre(EntityManagerInterface $em, Request $request, PaginatorInterface $paginator, $genreSlug, BreadCrumbsService $breadCrumbsService): Response
+    {
         $genre = $em->getRepository(Genre::class)->findOneBy(["slug" => $genreSlug]);
 
-	    $games = [];
+        if (!$genre) {
+            $this->addFlash("danger", 'Le genre n\'existe pas');
+            return $this->redirectToRoute("app_404");
+        }
 
-		$gamesLabel = [];
-
-		foreach($gamesDB as $game) {
-			$gamesLabel[] = $game->getLabel();
-		}
-
-		$gamesInfos = $callApiService->getInfosByGames($gamesLabel);
-		//dd($gamesInfos);
-
-	    foreach($gamesDB as $key => $game) {
-		    $games[] = ["game" => $game, "coverID" => $gamesInfos[$key]["cover"]["image_id"]];
-	    }
-
-	    //dd($games);
+        $games = $this->callApiService->getInfosByGames(["genre" => $genre->getSlug()]);
 
         // Pagination KNPPaginator
         $pagination = $paginator->paginate(
-          $games,
-          $request->query->get("page", 1), 9
+            $games, $request->query->get("page", 1), 6
         );
 
         $pagination->setCustomParameters(["align" => "center",
-          "size" => "small",
-          "style" => "bottom",
-          "span_class" => "whatever",]);
+            "size" => "small",
+            "style" => "bottom",
+            "span_class" => "whatever",]);
         // Fin pagination
 
-
-	    //dd($pagination);
-
-        /*$breadcrumbs->addRouteItem($genre->getLabel(), "app_show_game_genre", ['genreSlug' => $genreSlug]);
-            $breadcrumbs->prependRouteItem("Genres", "genre_list");
-            $breadcrumbs->prependRouteItem("Accueil", "app_home");*/
-
         $breadCrumbsService->BCGenerate([], [], [], ["label" => $genre->getLabel(),
-          "route" => "app_show_game_genre",
-          "params" => ["genreSlug" => $genreSlug],]);
+            "route" => "app_show_game_genre",
+            "params" => ["genreSlug" => $genre->getSlug()],]);
 
-        return $this->render("game/genre/show.html.twig", ["games" => $pagination,
-          "gameAvailable" => $games,
-          "genre" => $genre,
-          "description" => "Retrouvez toutes les informations concernant les jeux du genre " . $genre->getLabel(
-            ) . " sur K-Gaming.",]);
+        return $this->render("game/genre/show.html.twig", [
+            "games" => $pagination,
+            "gameAvailable" => $games,
+            "genre" => $genre,
+            "description" => "Retrouvez toutes les informations concernant les jeux du genre " . $genre->getLabel() . " sur K-Gaming."
+        ]);
     }
 
-    /*
-     * Méthodes permettant de récupérer une partie des informations d'un jeu grâce à l'API
-     */
-    public function getImageIDGame ($gameLabel): Response
+    public function getImageIDGame($gameLabel): Response
     {
         return $this->render(
-          "components/game/_game_image_id.html.twig",
-          ["gameInfos" => $this->callApiService->getCoverByGame($gameLabel),]
+            "components/game/_game_image_id.html.twig",
+            ["gameInfos" => $this->callApiService->getCoverByGame($gameLabel),]
         );
     }
-
-    public function getSummaryGame ($gameLabel): Response
-    {
-        return $this->render(
-          "components/game/_game_summary.html.twig",
-          ["gameInfos" => $this->callApiService->getSummaryByGame($gameLabel),]
-        );
-    }
-
-    public function getScreenshotsGame ($gameLabel): Response
-    {
-        return $this->render(
-          "components/game/_game_screenshots.html.twig",
-          ["gameInfos" => $this->callApiService->getScreenshotByGame($gameLabel),]
-        );
-    }
-
-    public function getVideosGame ($gameLabel): Response
-    {
-        return $this->render(
-          "components/game/_game_videos.html.twig", ["gameInfos" => $this->callApiService->getVideosByGame($gameLabel),]
-        );
-    }
-    // Fin méthodes API
 }
--- src/Service/CallApiService.php
+++ src/Service/CallApiService.php
@@ -2,69 +2,82 @@
 
 namespace App\Service;
 
+use App\Entity\Game;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\CachingHttpClient;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpKernel\HttpCache\Store;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class CallApiService extends AbstractController
 {
-    public function __construct (private readonly HttpClientInterface $client)
+    public function __construct(private HttpClientInterface $client, private $targetStore, private readonly EntityManagerInterface $em)
     {
+        $store = new Store($this->targetStore);
+        $this->client = HttpClient::create();
+        $this->client = new CachingHttpClient($this->client, $store);
     }
 
-	public function getInfosByGames (array $games): array
-	{
-		if(!empty($games)) {
-			$gamesString = implode('","', $games);
-			return $this->connectAPI('fields name, summary, cover.image_id, screenshots.image_id; where name = ("' . $gamesString . '");');
-		}
-		return [];
-	}
-
-    // Function to create connection with api (IGDB)
-    public function getCoverByGame ($game): array
+    public function getInfosByGames(array $options): array
     {
-        // Utilisation de la fonction connectAPI pour se connecter à l'API IGDB
-        return $this->connectAPI(// Requête à effectuer pour récupérer la cover du jeu remplaçant le nom du jeu par la variable $game
-          'fields name, cover.image_id; where name = "' . $game . '";'
-        );
+        $games = [];
+        if (!empty($options)) {
+
+            $gamesDB = $this->em->getRepository(Game::class)->findGamesByOptions($options);
+
+            $gamesSlug = [];
+            foreach ($gamesDB as $game) {
+                $gamesSlug[] = $game->getSlug();
+            }
+
+            $slugs = implode('","', $gamesSlug);
+
+            $gamesInfos = $this->connectAPI(
+                'fields name, slug, summary, cover.image_id, screenshots.image_id; where slug = ("' . $slugs . '"); limit 50;'
+            );
+
+            foreach ($gamesDB as $game) {
+                $gameName = $game->getLabel();
+
+                foreach ($gamesInfos as $info) {
+                    if ($info['name'] === $gameName) {
+                        $games[] = ["game" => $game, "infos" => $info];
+                    }
+                }
+            }
+
+            return $games;
+        }
+        return $games;
     }
 
-    // function to get cover of a game and using the function connectAPI to connect to the api
-    public function connectAPI ($data): array
+    // Function to create connection with api (IGDB) and get informations about games
+    public function connectAPI($data): array
     {
         // Effectue une requête HTTP avec la méthode POST sur l'URL de l'API IGDB
         $response = $this->client->request(// Méthode HTTP utilisée
-          "POST", // URL de l'API IGDB
-          "https://api.igdb.com/v4/games/", // Options de la requête
-          [// En-têtes de la requête (obligatoire)
-            "headers" => [// Client-ID et Authorization sont des en-têtes obligatoires pour se connecter à l'API IGDB
-              "Client-ID" => $this->getParameter("app.client_id"),
-              "Authorization" => $this->getParameter("app.authorization"),],
-              // Corps de la requête (obligatoire) avec la requête à effectuer grâce à la variable $data
-            "body" => $data,]
+            "POST", // URL de l'API IGDB
+            "https://api.igdb.com/v4/games/", // Options de la requête
+            [
+                // En-têtes de la requête (obligatoire)
+                "headers" => [
+                    // Client-ID et Authorization sont des en-têtes obligatoires pour se connecter à l'API IGDB
+                    "Client-ID" => $this->getParameter("app.client_id"),
+                    "Authorization" => $this->getParameter("app.authorization")
+                ],
+                // Corps de la requête (obligatoire) avec la requête à effectuer grâce à la variable $data
+                "body" => $data
+            ]
         );
 
         return $response->toArray();
     }
 
-    // function to get the summary of a game and using the function connectAPI to connect to the api
-    public function getSummaryByGame ($game): array
+    // function to get cover of a game and using the function connectAPI to connect to the api
+    public function getCoverByGame($game): array
     {
         // Utilisation de la fonction connectAPI pour se connecter à l'API IGDB
-        return $this->connectAPI(// Requête à effectuer pour récupérer le résumé du jeu remplaçant le nom du jeu par la variable $game
-          'fields name, summary; where name = "' . $game . '";'
-        );
-    }
-
-    // function to get screenshots of a game and using the function connectAPI to connect to the api
-    public function getScreenshotByGame ($game): array
-    {
-        return $this->connectAPI('fields name, screenshots.image_id; where name = "' . $game . '";');
-    }
-
-    // function to get videos of a game and using the function connectAPI to connect to the api
-    public function getVideosByGame ($game): array
-    {
-        return $this->connectAPI('fields name, videos.name, videos.video_id; where name = "' . $game . '";');
+        return $this->connectAPI('fields name, cover.image_id; where name = "' . $game . '";');
     }
 }
